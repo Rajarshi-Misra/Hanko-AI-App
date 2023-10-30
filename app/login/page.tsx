a3c8ec160e9e3e2533c772798cfa01b5408ce8dc
Rajarshi-Misra <rajarshimisra2100@gmail.com>
2023-10-30 14:10:16 +0530

    Trying to resolve CustomEvent not defined error

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,41 +2,47 @@
 import { useEffect, useState, useCallback } from "react";
 import { useRouter } from "next/navigation";
 import { register, Hanko } from "@teamhanko/hanko-elements";
+import dynamic from "next/dynamic";
+const HankoAuth = dynamic(
+  () => import("@/components/hanko-components/HankoAuth"),
+  { ssr: false }
+);
 
 const hankoApi = process.env.NEXT_PUBLIC_HANKO_API_KEY || "";
 export default function Login() {
-  const router = useRouter();
-  const [hanko, setHanko] = useState<Hanko>();
+  // const router = useRouter();
+  // const [hanko, setHanko] = useState<Hanko>();
 
-  useEffect(() => {
-    import("@teamhanko/hanko-elements").then(({ Hanko }) =>
-      setHanko(new Hanko(hankoApi ?? ""))
-    );
-  }, []);
+  // useEffect(() => {
+  //   import("@teamhanko/hanko-elements").then(({ Hanko }) =>
+  //     setHanko(new Hanko(hankoApi ?? ""))
+  //   );
+  // }, []);
 
-  const redirectAfterLogin = useCallback(() => {
-    router.replace("/dashboard");
-  }, [router]);
+  // const redirectAfterLogin = useCallback(() => {
+  //   router.replace("/dashboard");
+  // }, [router]);
 
-  useEffect(
-    () =>
-      hanko?.onAuthFlowCompleted(() => {
-        redirectAfterLogin();
-      }),
-    [hanko, redirectAfterLogin]
-  );
+  // useEffect(
+  //   () =>
+  //     hanko?.onAuthFlowCompleted(() => {
+  //       redirectAfterLogin();
+  //     }),
+  //   [hanko, redirectAfterLogin]
+  // );
 
-  useEffect(() => {
-    //
-    register(hankoApi ?? "").catch((error) => {
-      console.log(error);
-    });
-  }, []);
+  // useEffect(() => {
+  //   //
+  //   register(hankoApi ?? "").catch((error) => {
+  //     console.log(error);
+  //   });
+  // }, []);
 
   return (
     <div className="flex min-h-screen justify-center items-center bg-black">
       <div className="bg-white sm:p-5 rounded-2xl shadow-md">
-        <hanko-auth />
+        {/* <hanko-auth /> */}
+        <HankoAuth />
       </div>
     </div>
   );
