fd74351910a520a7952a10813054af1e049222dd
Rajarshi-Misra <rajarshimisra2100@gmail.com>
2023-10-31 16:46:53 +0530

    Progress Icon

--- components/Form.tsx
+++ components/Form.tsx
@@ -7,6 +7,7 @@ import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 import { useRouter } from "next/navigation";
 import { createClient } from "@supabase/supabase-js";
+import CircularProgress from "@mui/material/CircularProgress";
 const VisuallyHiddenInput = styled("input")({
   clip: "rect(0 0 0 0)",
   clipPath: "inset(50%)",
@@ -190,7 +191,10 @@ export const Form = () => {
           {!loading ? (
             <div className="font-bold text-xl">RENDER DESIGNS</div>
           ) : (
-            <div className="font-bold text-xl">PLEASE WAIT</div>
+            <div className="font-bold text-xl flex">
+              PLEASE WAIT
+              <CircularProgress />
+            </div>
           )}
         </Button>
       </div>
